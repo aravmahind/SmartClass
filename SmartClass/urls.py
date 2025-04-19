51a7ea60cee83f1aa5d1bd5a6d4407da73142477
Your Name <your.email@example.com>
2025-04-19 17:43:11 +0530

    Smartclass urls.py updated

--- SmartClass/urls.py
+++ SmartClass/urls.py
@@ -4,9 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('students.urls')),
-]
+     path('admin/', admin.site.urls),
+     path('', include('students.urls')),  # include app urls
+     path('', include('students.urls')),
+ ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
